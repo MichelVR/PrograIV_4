f8c1c4986a2bf562706968d3af7d51351fd022a3
Matias Rodriguez Corvalan <mrodriguez1293@icloud.com>
2016-11-24 10:57:45 -0300

    ProyectoIV

--- PrograIV_4/Startup.cs
+++ PrograIV_4/Startup.cs
@@ -1,8 +1,6 @@
-﻿using Microsoft.Owin;
-using Owin;
+﻿using Owin;
 
-[assembly: OwinStartupAttribute(typeof(PrograIV_4.Startup))]
-namespace PrograIV_4
+namespace IdentitySample
 {
     public partial class Startup
     {
