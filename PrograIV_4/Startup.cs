1bbef3ee37187d47ef168175078ed2712c5702fb
Matias Rodriguez Corvalan <mrodriguez1293@icloud.com>
2016-11-24 10:50:08 -0300

    Agregar archivos de proyecto.

---
+++ PrograIV_4/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(PrograIV_4.Startup))]
+namespace PrograIV_4
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
